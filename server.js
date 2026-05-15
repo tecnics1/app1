b38164538c74f93634611693c30d6f2ce804c1fa
Tecnics <tecnics1@gmail.com>
2026-05-15 17:47:16 +0200

    primera versio

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/missatge', (req, res) => {
+  res.json({
+    missatge: 'Hola des del backend!'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor executant-se a http://localhost:${PORT}`);
+});
